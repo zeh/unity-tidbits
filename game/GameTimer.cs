91084087a86ce036efa21889592e650c7c89f60a
zeh <zeh@zehfernando.com>
2014-09-07 19:14:00 -0400

    Added basic game manager classes

---
+++ game/GameTimer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using UnityEngine;
+
+class GameTimer {
+
+	// Properties
+	private bool isRunning;
+	private bool useRealTime;
+	private float timeElapsed;
+	private float currentTime;
+	private float lastTime;
+	private float timeScale;
+
+
+	// ================================================================================================================
+	// EXTENDED INTERFACE ---------------------------------------------------------------------------------------------
+
+	void Start() {
+		isRunning = false;
+		useRealTime = false;
+		timeElapsed = 0;
+		currentTime = 0;
+		lastTime = 0;
+		timeScale = 1;
+	}
+
+	void Update() {
+
+	}
+
+
+	// ================================================================================================================
+	// PUBLIC INTERFACE -----------------------------------------------------------------------------------------------
+
+	public void startTimer() {
+		if (!isRunning) {
+			isRunning = true;
+			lastTime = getSystemTime();
+		}
+	}
+
+	public void stopTimer() {
+		if (!isRunning) {
+			isRunning = false;
+		}
+	}
+
+	public void resetTimer() {
+		timeElapsed = 0;
+		currentTime = 0;
+		lastTime = getSystemTime();
+	}
+
+	public float getTime() {
+		// Return the time, in seconds
+		updateTimer();
+		return currentTime;
+	}
+
+
+	// ================================================================================================================
+	// INTERNAL INTERFACE ---------------------------------------------------------------------------------------------
+
+	private float getSystemTime() {
+		// Get the system time, in seconds
+		return useRealTime ? Time.realtimeSinceStartup : Time.time;
+	}
+
+	private void updateTimer() {
+		// Time since last Update()
+		float currTime = getSystemTime();
+
+		if (currTime > lastTime) {
+			timeElapsed = currTime - lastTime;
+
+			// If running, add the time elapsed to advance the timer
+			if (isRunning) currentTime += timeElapsed * timeScale;
+
+			// Store time so we can use it on the next update
+			lastTime = currTime;
+		}
+	}
+
+}
---
+++ game/SceneManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager {
+
+	// Properties
+	public string[] levelNames;
+	private int _currentLevel;
+	
+
+	// ================================================================================================================
+	// EXTENDED INTERFACE ---------------------------------------------------------------------------------------------
+
+	public void Start() {
+		// keep this object alive
+		_currentLevel = -1;
+	}
+
+
+	// ================================================================================================================
+	// PRIVATE INTERFACE ----------------------------------------------------------------------------------------------
+
+	private void loadLevel(string __sceneName) {
+		Application.LoadLevel(__sceneName);
+	}
+		
+	private void loadLevel(int __index) {
+		loadLevel(levelNames[__index]);
+	}
+
+
+	// ================================================================================================================
+	// ACCESSOR INTERFACE -----------------------------------------------------------------------------------------------
+
+	public int currentLevel {
+		get {
+			return _currentLevel;
+		}
+	}
+
+
+	// ================================================================================================================
+	// PUBLIC INTERFACE -----------------------------------------------------------------------------------------------
+
+	public void loadNextLevel() {
+		int nextLevel = (_currentLevel + 1) % levelNames.Length;
+		loadLevel(nextLevel);
+		_currentLevel = nextLevel;
+	}
+
+}
