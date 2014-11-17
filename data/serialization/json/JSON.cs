74270d7b5cf39f21c233334b442119497e6fa576
zeh <zeh@zehfernando.com>
2014-11-17 11:29:43 -0500

    Some utility functions for dictionary parsing

--- data/serialization/json/JSON.cs
+++ data/serialization/json/JSON.cs
@@ -187,6 +187,28 @@ public class JSON {
 		return encodeObject(input, prettyPrint);
 	}
 
+	// Utilitary functions for dictionary
+
+	public static Dictionary<string, object> getDictionary(Dictionary<string, object> input, string key, Dictionary<string, object> defaultValue = null) {
+		if (input.ContainsKey(key)) return (Dictionary<string, object>)input[key];
+		return defaultValue;
+	}
+
+	public static string getString(Dictionary<string, object> input, string key, string defaultValue = "") {
+		if (input.ContainsKey(key)) return (string)input[key];
+		return defaultValue;
+	}
+
+	public static bool getBoolean(Dictionary<string, object> input, string key, bool defaultValue = false) {
+		if (input.ContainsKey(key)) return (bool)input[key];
+		return defaultValue;
+	}
+
+	public static List<object> getList(Dictionary<string, object> input, string key, List<object> defaultValue = null) {
+		if (input.ContainsKey(key)) return (List<object>)input[key];
+		return defaultValue;
+	}
+
 
 	// ================================================================================================================
 	// INTERNAL STATIC INTERFACE --------------------------------------------------------------------------------------
