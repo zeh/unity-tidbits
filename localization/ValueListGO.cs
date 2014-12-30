a332d72e35ffebd90ad55c9329b822e274ba9b93
zeh <zeh@zehfernando.com>
2014-12-30 09:32:46 -0500

    Added a mono behavior to manage the valuelist

---
+++ localization/ValueListGO.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class ValueListGO:MonoBehaviour {
+
+	// Properties
+	public string filename;
+	public string name;
+
+
+	// ================================================================================================================
+	// MAIN EVENT INTERFACE -------------------------------------------------------------------------------------------
+
+	void Start() {
+		var valueList = new ValueList(name);
+		valueList.SetFromJSON(File.ReadAllText(filename));
+
+		//Debug.Log("====> " + ValueList.getInstance().GetString("language.systems.hepburn"));
+	}
+}
