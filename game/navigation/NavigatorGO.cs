88a129bd797e0f2bad110b13cc3106737d25504c
zeh <zeh@zehfernando.com>
2015-01-05 11:36:11 -0500

    Added classes to handle on-screen navigation (with the camera)

---
+++ game/navigation/NavigatorGO.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigatorGO:MonoBehaviour {
+
+	// Parameters
+	public NavigatorScene startingScene;
+
+	// Properties
+	private static NavigatorGO instance;
+
+	private bool isNavigating;
+	private NavigatorScene currentScene;
+
+	// ================================================================================================================
+	// MAIN EVENT INTERFACE -------------------------------------------------------------------------------------------
+
+	void Start() {
+		instance = this;
+		isNavigating = false;
+		if (startingScene != null) navigateTo(startingScene, true);
+	}
+
+	void Update() {
+
+	}
+
+	// ================================================================================================================
+	// PUBLIC INTERFACE -----------------------------------------------------------------------------------------------
+
+	public static NavigatorGO getInstance() {
+		return instance;
+	}
+
+	public void navigateTo(string gameObjectName) {
+		GameObject gameObject = GameObject.Find(gameObjectName);
+		if (gameObject != null && gameObject.GetComponent<NavigatorScene>() != null) {
+			navigateTo(gameObject.GetComponent<NavigatorScene>());
+		} else {
+			Debug.LogError("Game object with name [" + gameObjectName + "] and a NavigatorScene component not found!");
+		}
+	}
+
+	public void navigateTo(NavigatorScene newScene, bool immediate = false) {
+		// Navigates to a gameobject with the camera
+		//Debug.Log("navigating to " + gameObject + " @ " + gameObject.transform.position);
+
+		if (!isNavigating && newScene != null && newScene.cameraTarget != null && newScene != currentScene) {
+			//* .call(() => logDone("over"))
+
+			if (immediate) {
+				// Immediately show it
+				if (currentScene != null) currentScene.onStartedHiding();
+				newScene.onStartedShowing();
+				Camera.main.gameObject.transform.position = newScene.cameraTarget.transform.position;
+				if (currentScene != null) currentScene.onFinishedHiding();
+				newScene.onFinishedShowing();
+			} else {
+				// Animate to it
+				// Create tween
+				var tween = ZTween.use(Camera.main.gameObject);
+
+				// Call start functions
+				if (currentScene != null) tween.call(currentScene.onStartedHiding);
+				tween.call(newScene.onStartedShowing);
+
+				// Animate
+				tween.moveTo(newScene.cameraTarget.transform.position, 0.4f, Easing.backInOut);
+
+				// Call ending functions
+				if (currentScene != null) tween.call(currentScene.onFinishedHiding);
+				tween.call(newScene.onFinishedShowing);
+			
+				// Play the tween
+				tween.play();//.wait(1).call(Func).set("visible", false).play();
+			}
+
+			currentScene = newScene;
+		}
+	}
+}
---
+++ game/navigation/NavigatorScene.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigatorScene:MonoBehaviour {
+
+	// Properties
+	public GameObject cameraTarget;
+	public bool preferFullscreen;
+	public string trackingId;
+
+	private bool isActivated = true;
+	private bool disableRendering = false;
+
+	// ================================================================================================================
+	// EXTENDED INTERFACE ---------------------------------------------------------------------------------------------
+
+	void Start() {
+		deActivate();
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Escape) && isActivated) {
+			// Look for all "back" buttons...
+			var buttons = gameObject.GetComponentsInChildren<RoundedButtonNavigateTo>();
+			var acted = false;
+			foreach (var button in buttons) {
+				if (button.isBackEquivalent) {
+					acted = true;
+					button.performActionPublic();
+					break;
+				}
+			}
+			if (!acted) {
+				Debug.Log("No back, will quit!");
+				Application.Quit();
+			}
+		}
+	}
+
+	public virtual void onStartedShowing() {
+		// Changes screen
+		ApplicationChrome.statusBarState = ApplicationChrome.navigationBarState = ApplicationChrome.States.Visible;
+		ApplicationChrome.dimmed = preferFullscreen;
+
+		activate();
+
+		// Track page
+		//Debug.Log("Trying to track ::: [" + "navigation:screen:" + trackingId + "]");
+		GA.API.Design.NewEvent("navigation:screen:" + trackingId); 
+	}
+
+	public virtual void onFinishedShowing() {
+	}
+
+	public virtual void onStartedHiding() {
+	}
+
+	public virtual void onFinishedHiding() {
+		deActivate();
+	}
+
+	private void deActivate() {
+		//gameObject.SetActive(false);
+		//renderer.enabled = false;
+		//gameObject.SetActive(false);
+		if (isActivated) {
+			// Disable all rendering
+			if (disableRendering) {
+				var renderers = GetComponentsInChildren<Renderer>();
+				foreach (var renderer in renderers) {
+					renderer.enabled = false;
+				}
+			}
+
+			isActivated = false;
+		}
+	}
+
+	private void activate() {
+		//gameObject.SetActive(true);
+		//renderer.enabled = true;
+		//gameObject.SetActive(true);
+		if (!isActivated) {
+			// Disable all rendering
+			if (disableRendering) {
+				var renderers = GetComponentsInChildren<Renderer>();
+				foreach (var renderer in renderers) {
+					renderer.enabled = true;
+				}
+			}
+
+			isActivated = true;
+		}
+	}
+}
