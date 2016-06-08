785bb261d1f6bcdffaf7ab0ed296b842990f9229
Zeh Fernando <zeh@work.co>
2016-06-08 14:17:27 -0400

    AndroidUtils: avoid using Android classes on other platforms

--- application/AndroidUtils.cs
+++ application/AndroidUtils.cs
@@ -6,23 +6,27 @@
 /**
  * @author zeh fernando
  */
+using System;
+using UnityEngine;
+
 class AndroidUtils {
 
 	// Use:
 	// runOnAndroidUiThread(someMethod)
 	public static void runOnAndroidUiThread(Action target) {
 		// Calls an Android activity on the UI Thread (needed for some interactions with Views)
-		using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
-			using (var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity")) {
-				#if USE_ANDROID
-				activity.Call("runOnUiThread", new AndroidJavaRunnable(target));
-				#else
-				Debug.Log("[Android] Would call [" + target + "] later on the UI thread");
-				#endif
-				//string a = activity.Call<string>("someNomw", param);
-				//window.Call("setFlags", flagsValue, -1); // (int)0x7FFFFFFF
-				//window.Call("setStatusBarColor", unchecked((int)_statusBarColor)); // for an uint
+		#if USE_ANDROID
+			using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+				using (var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity")) {
+					activity.Call("runOnUiThread", new AndroidJavaRunnable(target));
+					// Other examples:
+					//string a = activity.Call<string>("someNomw", param);
+					//window.Call("setFlags", flagsValue, -1); // (int)0x7FFFFFFF
+					//window.Call("setStatusBarColor", unchecked((int)_statusBarColor)); // for an uint
+				}
 			}
-		}
+		#else
+			Debug.Log("[Android] Would call [" + target + "] later on the UI thread");
+		#endif
 	}
 }
