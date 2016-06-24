4045985d1246d02c923da041fa109f59be1c6422
Zeh Fernando <zeh@work.co>
2016-06-24 16:58:36 -0400

    GameObject: added utility components

---
+++ objects/MeshColliderCreator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode()]
+#endif
+
+public class MeshColliderCreator : MonoBehaviour {
+
+	// Automatically add mesh colliders to all child meshes
+
+	// TODO: actually remove this file
+
+	// Properties
+	public bool actOnChildren;
+
+	// ================================================================================================================
+	// MAIN EVENT INTERFACE -------------------------------------------------------------------------------------------
+
+	#if UNITY_EDITOR
+	void Start () {
+		createColliders();
+	}
+
+	void Update() {
+		if (!Application.isPlaying) createColliders();
+	}
+	#endif
+
+	// ================================================================================================================
+	// INTERNAL INTERFACE ---------------------------------------------------------------------------------------------
+
+	private void createColliders() {
+		GameObjectUtils.createMeshColliders(gameObject, actOnChildren);
+	}
+}
---
+++ objects/ModelResizer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode()]
+#endif
+
+public class ModelResizer:MonoBehaviour {
+
+	// Resizes a model to a desired maximum size
+	// TODO: Actually get rid of this file.
+
+	// Properties
+	public float desiredHeight;
+	public float desiredWidth;
+	public float desiredDepth;
+
+	// ================================================================================================================
+	// MAIN EVENT INTERFACE -------------------------------------------------------------------------------------------
+
+	#if UNITY_EDITOR
+	void Start () {
+		resize();
+	}
+
+	void Update() {
+		if (!Application.isPlaying) resize();
+	}
+	#endif
+
+	// ================================================================================================================
+	// INTERNAL INTERFACE ---------------------------------------------------------------------------------------------
+
+	private void resize() {
+		foreach (Transform child in transform) {
+			// Resize
+			GameObjectUtils.resizeToFit(child.gameObject, desiredWidth, desiredHeight, desiredDepth);
+			// Center in the bottom
+			GameObjectUtils.alignToVector(child.gameObject, transform.position, 0.5f, 0, 0.5f);
+		}
+	}
+}
